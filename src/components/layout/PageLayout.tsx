d8d185366f251f480bc69a5a85ba9a7f707aa449
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-10-18 23:21:54 +0300

    refactor(layout): removed parallax effect

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,38 +1,45 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
-import footerBgImg from "@/assets/images/footer/pexels-harrycunningham-3347324.jpg";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+// import footerBgImg from "@/assets/images/footer/pexels-harrycunningham-3347324.jpg";
+// import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 const PageLayout = () => {
   return (
     <div className="h-[100%]">
       <Navbar />
-      <Parallax pages={1.5}>
+      <div className="px-8 pb-4 sm:px-12 md:px-12">
+        <Outlet />
+      </div>
+      <div className="h-[30%] px-8 pt-4 sm:px-12 md:px-12 m-auto bg-neutral-950 bg-opacity-85">
+        <Footer />
+      </div>
+      {/* <Parallax pages={1.5}>
         <ParallaxLayer
           offset={0}
           factor={1.5}
           speed={0.8}
-          className="px-8 pb-4 sm:px-12 md:px-12 lg:px-12"
+          className="px-8 pb-4 sm:px-12 md:px-12"
         >
           <Outlet />
         </ParallaxLayer>
         <ParallaxLayer
-          offset={1}
-          speed={0}
+          offset={1.2}
+          speed={-0.5}
           factor={1}
           style={{
             background: `url(${footerBgImg}) rgba(0, 0, 0, 0.75)`,
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
             backgroundBlendMode: "multiply",
+            zIndex: -10,
           }}
         >
-          <div className="h-[30%] px-8 pt-4 sm:px-12 md:px-12 lg:px-12 m-auto">
+          <div className="h-[30%] px-8 sm:px-12 md:px-12">
             <Footer />
           </div>
         </ParallaxLayer>
-      </Parallax>
+      </Parallax> */}
     </div>
   );
 };
